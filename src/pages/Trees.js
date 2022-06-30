3b626f188a3b4e8f21821c7a62f8542b7819f9bd
arshedgithub <arshedahmedmail@gmail.com>
2022-06-30 08:37:24 +0530

    create tree modal

--- src/pages/Trees.js
+++ src/pages/Trees.js
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Content } from "antd/lib/layout/layout";
 import { Table, Row, Col } from "antd";
-import axios from "axios";
 import service from "./../services/data-service";
 import "antd/dist/antd.css";
 import {
   Modal,
   Button,
   Card,
-  Form,
-  Input,
   Typography,
   DatePicker,
   Space,
 } from "antd";
-
-import { MdEmail, MdPhone } from "react-icons/md";
-
-import moment from "moment";
 
 const { Title } = Typography;
 
@@ -29,44 +22,32 @@ const Trees = () => {
 
   const columns = [
     {
-      title: "REGISTRATION NUMBER",
-      key: "register no",
-      dataIndex: "register no",
+      title: "UNIT TREE NUMBER",
+      key: "unit tree no",
+      dataIndex: "unit tree no",
       render: (index, record) => (
         <>
-          <Title level={5}>
-            {record.landOwnerRegisterNo}
-          </Title>
+          <Title level={5}>{record.lifeForceUnitTreeNo}</Title>
         </>
       ),
     },
     {
-      title: "LATITUDE",
-      key: "latitude",
-      dataIndex: "latitude",
-      render: (index, record) => (
-        <>
-          <Title level={5}>{record.latitude}</Title>
-        </>
-      ),
-    },
-    {
-      title: "LONGTITUDE",
-      key: "longtitude",
-      dataIndex: "longtitude",
-      render: (index, record) => (
-        <>
-          <Title level={5}>{record.longitude}</Title>
-        </>
-      ),
-    },
-    {
-      title: "TREEE SPECIE",
+      title: "TREE SPECIE",
       key: "tree specie",
       dataIndex: "tree specie",
       render: (index, record) => (
         <>
           <Title level={5}>{record.treeSpecies}</Title>
+        </>
+      ),
+    },
+    {
+      title: "LANDOWNER REGISTRATION NUMBER",
+      key: "landowner reistration no",
+      dataIndex: "landowner reistration no",
+      render: (index, record) => (
+        <>
+          <Title level={5}>{record.landOwnerRegisterNo}</Title>
         </>
       ),
     },
@@ -80,21 +61,16 @@ const Trees = () => {
         </>
       ),
     },
-    // {
-    //   title: "Edit User",
-    //   dataIndex: "id",
-    //   key: "id",
-    //   render: (index, record) => (
-    //     <Button
-    //       type="primary"
-    //       onClick={() => {
-    //         // showModal(record);
-    //       }}
-    //     >
-    //       Edit
-    //     </Button>
-    //   ),
-    // },
+    {
+      title: "AUDITOR ID",
+      key: "auditor id",
+      dataIndex: "auditor id",
+      render: (index, record) => (
+        <>
+          <Title level={5}>{record.creatorID}</Title>
+        </>
+      ),
+    },
   ];
 
   useEffect(async () => {
@@ -127,41 +103,24 @@ const Trees = () => {
       )
     );
   }, []);
-  // };
 
   const [isModalVisible, setIsModalVisible] = useState(false);
-  // const [setLoading] = useState(false);
-
-  // const showModal = (record) => {
-  //   console.log(record);
-  //   setFullName(record.fullName);
-  //   setmodaldata(record);
-  //   console.log(modaldata, fullName);
-  //   setIsModalVisible(true);
-  // };
-
-  // const handleOk = () => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //     setIsModalVisible(false);
-  //   }, 3000);
-  // };
-
+const {
+          treeID,
+          creatorID,
+          landOwnerID,
+          landOwnerRegisterNo,
+          lifeForceUnitTreeNo,
+          treeSpecies,
+          dateofPlanting,
+          createdAt,
+          longitude,
+          latitude,
+        } = modaldata
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  const layout = {
-    labelCol: {
-      span: 6,
-    },
-    wrapperCol: {
-      span: 16,
-    },
-  };
 
-  console.log(data
-    );
   return (
     <>
       <Content>
@@ -170,198 +129,59 @@ const Trees = () => {
             <Card
               bordered={false}
               className="criclebox tablespace mb-24"
-              title="Auditors"
+              title="Trees"
             >
-              <Table dataSource={data} columns={columns} />
+              <Table
+                dataSource={data}
+                columns={columns}
+                onRow={(record, rowIndex) => {
+                  return {
+                    onClick: () => {
+                      setIsModalVisible(true)
+                      console.log(record, rowIndex);
+                      setmodaldata(record)
+                    },
+                  };
+                }}
+              />
             </Card>
           </Col>
         </Row>
       </Content>
+
       <Modal
-        title="Update Auditor"
+        title="Tree"
         visible={isModalVisible}
-        // onOk={handleOk}
         onCancel={handleCancel}
         destroyOnClose
         footer={[
           <Button
-            type="danger"
-            onClick={() => {
-              // handleDeleteClick(modaldata.id);
-            }}
+            // key="submit"
+            type="primary"
+            onClick={handleCancel}
           >
-            Delete Auditor
+            Update
+          </Button>,
+          <Button
+            type="danger"
+            onClick={handleCancel}
+          >
+            Delete
           </Button>,
           <Button key="back" onClick={handleCancel}>
             Cancel
           </Button>,
-          <Button
-            key="submit"
-            type="primary"
-            // onClick={() => handleUpdatelick(modaldata.id)}
-          >
-            Save
-          </Button>,
         ]}
       >
-        <Form {...layout}>
-          <Form.Item
-            name="fullName"
-            label="Full Name"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input
-              name="fullName"
-              placeholder={modaldata.fullName}
-              value={modaldata.fullName}
-              onChange={(event) => {
-                setmodaldata({
-                  ...modaldata,
-                  fullName: event.target.value,
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            name="qualification"
-            label="Qualification"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input
-              name="qualification"
-              placeholder={modaldata.qualification}
-              value={modaldata.qualification}
-              onChange={(event) => {
-                setmodaldata({
-                  ...modaldata,
-                  qualification: event.target.value,
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            name="contactNumber"
-            label="Contact Number"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input
-              name="contactNumber"
-              placeholder={modaldata.contactNumber}
-              value={modaldata.contactNumber}
-              onChange={(event) => {
-                setmodaldata({
-                  ...modaldata,
-                  contactNumber: event.target.value,
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            label="Email"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input
-              name="email"
-              placeholder={modaldata.email}
-              value={modaldata.email}
-              onChange={(event) => {
-                setmodaldata({
-                  ...modaldata,
-                  email: event.target.value,
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            name="address"
-            label="Address"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input
-              name="address"
-              placeholder={modaldata.address}
-              value={modaldata.address}
-              onChange={(event) => {
-                setmodaldata({
-                  ...modaldata,
-                  address: event.target.value,
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            name="type"
-            label="User Type"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input
-              name="type"
-              placeholder={modaldata.type}
-              value={modaldata.type}
-              onChange={(event) => {
-                setmodaldata({
-                  ...modaldata,
-                  type: event.target.value,
-                });
-              }}
-            />
-          </Form.Item>
-          <Form.Item
-            name="dob"
-            label="dob"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            {/* <Input type= "date" name="dob" placeholder={modaldata.DOB} value={modaldata.DOB}
-                  onChange={(event) => {
-                    setmodaldata({
-                      ...modaldata,
-                      DOB: event.target.value
-                    })
-                  }}/> */}
-            <Space direction="vertical">
-              <DatePicker
-                name="dob"
-                value={moment(modaldata.DOB)}
-                onChange={(date, datestring) => {
-                  console.log(date.date());
-                  setmodaldata({
-                    ...modaldata,
-                    DOB: datestring,
-                  });
-                }}
-              />
-            </Space>
-          </Form.Item>
-        </Form>
+        <div>map</div>
+        <div>Tree ID : &nbsp;&nbsp;<b>{treeID}</b></div>
+        <div>Tree species : &nbsp;&nbsp;<b>{treeSpecies}</b></div>
+        <div>LifeForce Unit Tree No : &nbsp;&nbsp;<b>{lifeForceUnitTreeNo}</b></div>
+        <div>landOwner Register No : &nbsp;&nbsp;<b>{landOwnerRegisterNo}</b></div>
+        <div>landOwner ID : &nbsp;&nbsp;<b>{landOwnerID}</b></div>
+        <div>Auditor ID : &nbsp;&nbsp;<b>{creatorID}</b></div>
+        <div>Date of planting : &nbsp;&nbsp;<b>{dateofPlanting}</b></div>
+        
       </Modal>
     </>
   );
