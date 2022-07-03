aad9c8a8325bb0633676cb125c5d5cafe4b21c8b
RimazRizkan <rimaz660066@gmail.com>
2022-07-03 17:33:34 +0530

    Calling API endpoint from services folder

--- src/pages/TreeSpecies.js
+++ src/pages/TreeSpecies.js
@@ -19,6 +19,7 @@ import { useState, useEffect } from "react";
 import {
   SearchOutlined,
 } from "@ant-design/icons";
+import { treeSpeciesApi } from "../services/tree-species-service";
 
 
 
@@ -137,13 +138,7 @@ function TreeSpecies() {
   };
 
   useEffect(() => {
-    const headers = {
-      "x-auth-token":
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI5IiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjUxMzAwNDAzfQ.c2TZs11tgHna5irUHCaehVOGzup6YHE-SnTk9G25rtk",
-    };
-
-    axios
-      .get("http://ec2-13-229-44-15.ap-southeast-1.compute.amazonaws.com:4000/species/", {headers})
+    treeSpeciesApi()
       .then((res) => {
         setData(res.data.Result);
       })
