ea6ea2592eb8dc4d2bfd13526ff4e6c05247f280
thenukagunawardana <thenukanisal@gmail.com>
2022-08-01 10:55:40 +0530

    Added Messages To Auditor page

--- src/services/auditor-service.js
+++ src/services/auditor-service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Swal from "sweetalert2";
 
 export default function AuditorService() {
   const http = axios.create({
@@ -6,7 +7,8 @@ export default function AuditorService() {
       "http://ec2-13-229-44-15.ap-southeast-1.compute.amazonaws.com:4000",
     headers: {
       "Content-type": "application/json",
-      "x-auth-token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjQ1NTU1NTEzfQ.Kv2cEkCU-F9w_Gd_ajB2zfiUW66G6WPg7dPznedIRC0',
+      "x-auth-token":
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjQ1NTU1NTEzfQ.Kv2cEkCU-F9w_Gd_ajB2zfiUW66G6WPg7dPznedIRC0",
     },
   });
 
@@ -16,17 +18,31 @@ export default function AuditorService() {
   }
 
   async function updateAuditors(user) {
-    const data = await http.put("/users/updateAll/" + user.id,user).then((res) => res);
-    console.log(data);
+    const data = await http
+      .put("/users/updateAll/" + user.id, user)
+      .then((res) => res);
+      console.log(data.status === 200);
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Updated Successfully",
+      showConfirmButton: true,
+      timer: 2500,
+    });
   }
 
   async function deleteAuditors(Id) {
-    const data = await http.put("/users/deleteUser/" + Id,{}).then((res) => res);
-    console.log(
-      data.status === 200
-        ? data.data.message
-        : "Oops! something went wrong when deleting Auditors"
-    );
+    const data = await http
+      .put("/users/deleteUser/" + Id, {})
+      .then((res) => res);
+      console.log(data.status === 200);
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Deleted Successfully",
+      showConfirmButton: true,
+      timer: 2500,
+    });
   }
   return {
     getAuditors,
