b6b76a732523299d9e4130dfd53feb5019ea316d
neranjanpriyashantha <85043952+neranjanpriyashantha@users.noreply.github.com>
2022-08-06 07:27:55 +0530

    Changed AWS host

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -7,7 +7,7 @@ export default function AuthService() {
   const [, setCookie] = useCookies(['token']);
 
 const http = axios.create({
-  baseURL: "http://ec2-13-229-44-15.ap-southeast-1.compute.amazonaws.com:4000",
+  baseURL: "http://ec2-13-250-22-64.ap-southeast-1.compute.amazonaws.com:4000",
   headers: {
     "Content-type": "application/json",
   }});
--- src/services/landowner-service.js
+++ src/services/landowner-service.js
@@ -6,7 +6,7 @@ export default function DataService() {
 
   const http = axios.create({
     baseURL:
-      "http://ec2-13-229-44-15.ap-southeast-1.compute.amazonaws.com:4000",
+      "http://ec2-13-250-22-64.ap-southeast-1.compute.amazonaws.com:4000",
     headers: {
       "Content-type": "application/json",
       "x-auth-token": cookies.token,
