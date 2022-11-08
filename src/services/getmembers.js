68a3b80ac83ad8c1b3fe303a0064abf433ae6c53
Chamal Dissanayake <65418556+chamal-dissanayake@users.noreply.github.com>
2022-11-09 00:58:54 +0530

    change the database

--- src/services/getmembers.js
+++ src/services/getmembers.js
@@ -4,8 +4,8 @@ export default function memberService() {
     
     const http = axios.create({
       baseURL:
-        // "http://ec2-13-250-22-64.ap-southeast-1.compute.amazonaws.com:4000",
-        "http://localhost:4000",
+        "http://ec2-13-250-22-64.ap-southeast-1.compute.amazonaws.com:4000",
+        // "http://localhost:4000",
       headers: {
         "Content-type": "application/json",
       
